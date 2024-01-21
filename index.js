00948dcc453a802eb0396733d83f922f92ff20a5
Frederick-Ye <yenainglinn7@gmail.com>
2024-01-21 19:28:03 +0630

    set up website

---
+++ index.js
@@ -0,0 +1,65 @@
+import {
+  tasks,
+  taskInput,
+  clearBtn,
+  addBtn,
+  taskContainer,
+  resetBtn,
+} from './src/variables.js';
+
+const addTask = () => {
+  const newTask = {
+    description: taskInput.value,
+    index: tasks.length + 1,
+    checkboxId: tasks.length + 1,
+    completed: false
+  };
+  tasks.push(newTask);
+  saveTasksToLocalStorage();
+  displayTask();
+  taskInput.value = '';
+};
+
+const removeAll = () => {
+  tasks = [];
+  saveTasksToLocalStorage();
+  displayTask();
+};
+
+const removeCheckedTasks = () => {
+  tasks = tasks.filter(task => !document.getElementById(`checkbox_${task.checkboxId}`).checked);
+  tasks.forEach((task, index) => {
+    task.index = index + 1;
+    task.checkboxId = index + 1;
+  });
+  saveTasksToLocalStorage();
+  displayTask();
+};
+
+const displayTask = () => {
+  taskContainer.innerHTML = '';
+
+  tasks.forEach(task => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `<div>
+      <input class="checkbox" id="checkbox_${task.checkboxId}" type="checkbox"/>
+      ${task.description}
+    </div>
+    <div><i class="bx bx-dots-vertical-rounded"></i></div>`;
+    const checkbox = listItem.querySelector(`.checkbox`);
+    checkbox.addEventListener('change', () => {
+      task.completed = checkbox.checked;
+      saveTasksToLocalStorage();
+    });
+    taskContainer.appendChild(listItem);
+  });
+};
+
+const saveTasksToLocalStorage = () => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+addBtn.addEventListener('click', addTask);
+resetBtn.addEventListener('click', removeAll);
+clearBtn.addEventListener('click', removeCheckedTasks);
+displayTask();
---
+++ src/variables.js
@@ -0,0 +1,8 @@
+export let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+export const taskInput = document.querySelector('.addListField');
+export const clearBtn = document.getElementById('clearBtn');
+export const addBtn = document.querySelector('.bx-list-plus');
+export const taskContainer = document.querySelector('.dynamicContent');
+export const resetBtn = document.querySelector('.bx-recycle');
+
+export * from './variables.js';
