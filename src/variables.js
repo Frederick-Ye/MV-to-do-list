5421179694dff29e4b6cb24ae17a28c706699028
Frederick-Ye <yenainglinn7@gmail.com>
2024-01-21 20:25:09 +0630

    fix linters

--- src/variables.js
+++ src/variables.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/export */
 export let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 export const taskInput = document.querySelector('.addListField');
 export const clearBtn = document.getElementById('clearBtn');
@@ -7,4 +6,3 @@ export const taskContainer = document.querySelector('.dynamicContent');
 export const resetBtn = document.querySelector('.bx-recycle');
 
 export * from './variables.js';
-/* eslint-enable import/export */
